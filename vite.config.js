9088b4eeb86c6a834a5a43a8b90fd87761711583
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-02 00:36:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/keyng-david/KeyngKoin/',
+  base: '/KeyngKoin/',
   plugins: [react()],
   server: {
     open: false,
