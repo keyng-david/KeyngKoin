3bdc7989c2fa2c36f282e6fd802ecfc288ab4ef9
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-02 09:46:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/KeyngKoin/',  // Ensure this matches your GitHub repository name
+  base: '/KeyngKoin/',
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: '/index.html',  // Ensure the correct input file
+      input: 'index.html',
     },
   },
   server: {
     open: false,
     http: true,
     ssr: false,
+    historyApiFallback: true, // Add this line
   },
 });
