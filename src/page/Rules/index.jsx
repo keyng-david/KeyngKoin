f2d76f489285a2beaf82dbb320b0b9a49dcb7038
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-02 14:07:19 +0100

    Update index.jsx

--- src/page/Rules/index.jsx
+++ src/page/Rules/index.jsx
@@ -13,31 +13,23 @@ function Rules() {
       <div className="body">
         <section>
           <h3>Game Rule</h3>
-          <p>The player controls the length of the rod by long pressing the screen, so that when the rod falls clockwise, it will hit the next platform to ensure that the villain can pass through the ravine safely, otherwise the game will end if it fails</p>
-
-<p>When the player obtains the best score, the system prompts to save the score and consumes <b>10</b> Keyng Koins. If not saved, it will not be consumed</p>
+          <p>The player controls the length of the rod by long pressing the screen, so that when the rod falls clockwise, it will hit the next platform to ensure that the villain can pass through the ravine safely, otherwise the game will end if it fails.</p>
+          <p>When the player obtains the best score, the system prompts to save the score and consumes <b>10</b> Keyng Koins. If not saved, it will not be consumed.</p>
         </section>
         <section>
-          <p>The system performs this operation once a day at <b>00:00</b>After settlement, all the Keyng Koins consumed today will be awarded to all the top players in the ranking<b>50%<b>Players</p>
-         </section>
-         <section>
-           <h3>Reward details</h3>
-           <p>All daily income is returned to players</p>
-
-<p>The top <b>1000</b> players will share <b>10%</b> of all Mined Keyng Koins.</p>
-
-<p>The top <b>30%</b> of players will get an extra tie Share <b>15%</b></p> of Mined Coins
-
-<p>The top <b>15%</b> of players will get an extra tie Share <b>15%</b></p> of all Keyng Koins
-
-<p>The top <b>5%</b> players will get an extra share All gold coins<b>10%</b></p>
-
-<p>The third player gets all additional gold coins<b>2%</b></p>
-
-<p>The second player gets all additional gold coins<b>3%</b></p>
-
-
-<p>The first player gets all additional gold coins<b>5%</b></p>
+          <p>The system performs this operation once a day at <b>00:00</b>. After settlement, all the Keyng Koins consumed today will be awarded to all the top players in the ranking.</p>
+          <p><b>50%</b> of players will receive rewards.</p>
+        </section>
+        <section>
+          <h3>Reward details</h3>
+          <p>All daily income is returned to players.</p>
+          <p>The top <b>1000</b> players will share <b>10%</b> of all mined Keyng Koins.</p>
+          <p>The top <b>30%</b> of players will get an extra tie share of <b>15%</b> of mined coins.</p>
+          <p>The top <b>15%</b> of players will get an extra tie share of <b>15%</b> of all Keyng Koins.</p>
+          <p>The top <b>5%</b> of players will get an extra share of all gold coins: <b>10%</b>.</p>
+          <p>The third player gets an additional <b>2%</b> of all gold coins.</p>
+          <p>The second player gets an additional <b>3%</b> of all gold coins.</p>
+          <p>The first player gets an additional <b>5%</b> of all gold coins.</p>
         </section>
       </div>
     </div>
